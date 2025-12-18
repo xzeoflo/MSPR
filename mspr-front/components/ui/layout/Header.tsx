bdb3c25120bfec899b6f1f2cf5d4a2380c45ceab
alexkjzz <alexis.lamonnier@decathlon.com>
2025-12-18 09:10:30 +0100

    fix: fixed colors

--- mspr-front/components/ui/layout/Header.tsx
+++ mspr-front/components/ui/layout/Header.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 
 export default function Header() {
 
-  const actionClasses = "flex items-center justify-center w-10 h-10 rounded-full text-stone-400 hover:bg-neutral-800 hover:text-white transition-colors duration-200 cursor-pointer";
+  const actionClasses = "flex items-center justify-center w-10 h-10 rounded-full text-stone-400 hover:bg-stone-800 hover:text-white transition-colors duration-200 cursor-pointer";
 
   return (
     <header className="w-full h-16 flex items-center justify-between bg-neutral-950 text-white p-4 border-b border-neutral-800 shadow-xl">
