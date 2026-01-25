d1842ab1755b31cdc432b21ba2d6e6fe11f16dfe
alexkjzz <alex.lam@orange.fr>
2026-01-25 23:35:26 +0100

    feat: hudge change dashboard

--- mspr-front/app/(dashboard)/page.tsx
+++ mspr-front/app/(dashboard)/page.tsx
@@ -1,17 +1,40 @@
-import CardData from "@/components/ui/card/CardData";
+import { AppSidebar } from "@/components/app-sidebar"
+import { ChartAreaInteractive } from "@/components/chart-area-interactive"
+import { DataTable } from "@/components/data-table"
+import { SectionCards } from "@/components/section-cards"
+import { SiteHeader } from "@/components/site-header"
+import {
+  SidebarInset,
+  SidebarProvider,
+} from "@/components/ui/sidebar"
 
-export default function DashboardPage() {
+import data from "./data.json"
+
+export default function Page() {
   return (
-    <div className="flex flex-col gap-4">
-      <CardData title="Data" description="Graph de la data">
-        Composant data
-      </CardData>
-      <CardData title="Data" description="Graph de la data">
-        Composant data
-      </CardData>
-      <CardData title="Data" description="Graph de la data">
-        Composant data
-      </CardData>
-    </div>
-  );
+    <SidebarProvider
+      style={
+        {
+          "--sidebar-width": "calc(var(--spacing) * 72)",
+          "--header-height": "calc(var(--spacing) * 12)",
+        } as React.CSSProperties
+      }
+    >
+      <AppSidebar variant="inset" />
+      <SidebarInset>
+        <SiteHeader />
+        <div className="flex flex-1 flex-col">
+          <div className="@container/main flex flex-1 flex-col gap-2">
+            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+              <SectionCards />
+              <div className="px-4 lg:px-6">
+                <ChartAreaInteractive />
+              </div>
+              <DataTable data={data} />
+            </div>
+          </div>
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  )
 }
--- mspr-front/components/ui/layout/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="w-full p-2 text-secondary">
-      <div className="flex justify-center">
-        <p className="text-sm">© 2026 MSPR</p>
-      </div>
-    </footer>
-  )
-}
