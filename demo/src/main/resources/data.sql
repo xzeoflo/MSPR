b204e3f079ea82871312312d4117e604cc334efb
alexkjzz <alex.lam@orange.fr>
2026-01-31 10:59:04 +0100

    Clean: ignore IDE and build files

--- demo/src/main/resources/data.sql
+++ demo/src/main/resources/data.sql
@@ -14,7 +14,6 @@ INSERT INTO users (email, password, firstname, lastname, role, partner_brand, su
 ('client1.nike@test.com', '$2a$10$8.UnVuG9HHgffUDAlk8q2OuVGkqEnLPz0zke.6vNAnv3N.8p.EomG', 'Paul', 'Runner', 'CLIENT', 'Nike', 'FREEMIUM'),
 ('client2.nike@test.com', '$2a$10$8.UnVuG9HHgffUDAlk8q2OuVGkqEnLPz0zke.6vNAnv3N.8p.EomG', 'Julie', 'Fitness', 'CLIENT', 'Nike', 'PREMIUM'),
 ('client.adidas@test.com', '$2a$10$8.UnVuG9HHgffUDAlk8q2OuVGkqEnLPz0zke.6vNAnv3N.8p.EomG', 'Kevin', 'Yoga', 'CLIENT', 'Adidas', 'FREEMIUM');
-
 -- =============================================
 -- 2. WORKOUTS
 -- =============================================
@@ -24,7 +23,6 @@ INSERT INTO workouts (title, description, difficulty, partner_brand, workout_typ
 ('Adidas Cardio Flow', 'Endurance and breathing', 'Beginner', 'Adidas', 'CARDIO'),
 ('Nike Core Express', 'Abs workout', 'Beginner', 'Nike', 'CORE'),
 ('Adidas Strength 101', 'Basic full body strength', 'Intermediate', 'Adidas', 'STRENGTH');
-
 -- =============================================
 -- 3. EXERCISES
 -- =============================================
@@ -33,22 +31,18 @@ INSERT INTO exercises (name, description, duration_in_seconds, repetitions, sets
 ('Burpees', 'Full body cardio', 60, 15, 3, 45, 'High', 1, 1, 'CARDIO'),
 ('Mountain Climbers', 'Core and cardio', 45, 40, 3, 30, 'Medium', 2, 1, 'CARDIO'),
 ('Sprints', 'Max speed', 30, 1, 5, 50, 'Very High', 3, 1, 'CARDIO');
-
 -- Pour Workout 2 (Power Lift Nike)
 INSERT INTO exercises (name, description, duration_in_seconds, repetitions, sets, calories_burned, intensity_level, sequence_order, workout_id, exercise_type) VALUES
 ('Deadlift', 'Back and legs', 120, 5, 5, 80, 'Very High', 1, 2, 'STRENGTH'),
 ('Bench Press', 'Chest power', 120, 8, 4, 60, 'High', 2, 2, 'STRENGTH');
-
 -- Pour Workout 3 (Adidas Cardio Flow)
 INSERT INTO exercises (name, description, duration_in_seconds, repetitions, sets, calories_burned, intensity_level, sequence_order, workout_id, exercise_type) VALUES
 ('Jumping Jacks', 'Warmup cardio', 60, 50, 3, 25, 'Low', 1, 3, 'CARDIO'),
 ('Plank', 'Static hold', 60, 0, 3, 15, 'Medium', 3, 3, 'CORE');
-
 -- Pour Workout 4 (Nike Core)
 INSERT INTO exercises (name, description, duration_in_seconds, repetitions, sets, calories_burned, intensity_level, sequence_order, workout_id, exercise_type) VALUES
 ('Crunches', 'Upper abs', 60, 25, 3, 20, 'Medium', 1, 4, 'CORE'),
 ('Leg Raises', 'Lower abs', 60, 15, 3, 20, 'Medium', 2, 4, 'CORE');
-
 -- Pour Workout 5 (Adidas Strength)
 INSERT INTO exercises (name, description, duration_in_seconds, repetitions, sets, calories_burned, intensity_level, sequence_order, workout_id, exercise_type) VALUES
 ('Squats', 'Leg strength', 90, 15, 4, 40, 'Medium', 1, 5, 'STRENGTH'),
