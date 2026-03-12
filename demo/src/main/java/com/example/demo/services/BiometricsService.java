8895350291e402f6c4be2566dadc0e6bebeb7e75
xzeoflo <151619925+xzeoflo@users.noreply.github.com>
2026-03-12 21:44:48 +0100

    MSPR-22: added meal and biometrics crud + added eat for join meal to user

--- demo/src/main/java/com/example/demo/services/BiometricsService.java
+++ demo/src/main/java/com/example/demo/services/BiometricsService.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
